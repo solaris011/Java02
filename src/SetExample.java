2a1de85eb724796f48ad3ba34020f3fa4a7418ee
Saul Flores <solaris0250@gmail.com>
2025-06-09 16:40:46 -0600

    Borrar todo

--- src/SetExample.java
+++ src/SetExample.java
@@ -4,28 +4,6 @@ import java.util.Set;
 
 public class SetExample {
     public static void main(String[] args) {
-        Set<String> uniqueItems = new HashSet<>();
-
-        uniqueItems.add("Apple");
-        uniqueItems.add("Banana");
-        uniqueItems.add("Orange");
-        boolean added = uniqueItems.add("Apple"); 
-
-        System.out.println("Was 'Apple' added again? " + added);
-        System.out.println("Set size: " + uniqueItems.size());
-
-        for (String item : uniqueItems) {
-            System.out.println("For-each: " + item);
-        }
-
-        Iterator<String> it = uniqueItems.iterator();
-        while (it.hasNext()) {
-            String item = it.next();
-            if ("Banana".equals(item)) {
-                it.remove(); 
-            }
-        }
-
-        uniqueItems.forEach(item -> System.out.println("Lambda: " + item));
+      
     }
 }
